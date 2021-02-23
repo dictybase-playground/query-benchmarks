9c971347bdc6c919485ac036a154499b05df63a5
wildlifehexagon <crossthebreeze@gmail.com>
2021-02-23 16:37:44 -0600

    refactor: move query into separate function

--- internal/app/query.go
+++ internal/app/query.go
@@ -1,13 +1,12 @@
 package app
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"time"
 
 	anno "github.com/dictyBase/go-genproto/dictybaseapis/annotation"
 	stock "github.com/dictyBase/go-genproto/dictybaseapis/stock"
+	"github.com/dictybase-playground/query-benchmarks/internal/query"
 	"github.com/urfave/cli"
 	"google.golang.org/grpc"
 )
@@ -18,52 +17,36 @@ func TimeQuery(c *cli.Context) error {
 	aport := c.String("annotation-grpc-port")
 	aconn, err := grpc.Dial(fmt.Sprintf("%s:%s", ahost, aport), grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		return cli.NewExitError(
+			fmt.Sprintf("cannot connect to annotation grpc microservice %s", err),
+			2,
+		)
 	}
 	defer aconn.Close()
 	ann := anno.NewTaggedAnnotationServiceClient(aconn)
 	// initialize stock client
 	shost := c.String("stock-grpc-host")
-	sport := c.String("stock-grpc-host")
+	sport := c.String("stock-grpc-port")
 	sconn, err := grpc.Dial(fmt.Sprintf("%s:%s", shost, sport), grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		return cli.NewExitError(
+			fmt.Sprintf("cannot connect to stock grpc microservice %s", err),
+			2,
+		)
 	}
 	defer sconn.Close()
 	st := stock.NewStockServiceClient(sconn)
-	ctx := context.Background()
-
-	// to get non-gwdi strains "name=~GWDI"
-	start := time.Now()
-	avail := 0
-	unavail := 0
-	for v := 10; v <= 20; v += 10 {
-		start := time.Now()
-		strains, err := st.ListStrains(ctx, &stock.StockParameters{Cursor: 0, Limit: 10, Filter: "name!~GWDI;label!=AX4"})
-		if err != nil {
-			panic(err)
-		}
-		for _, n := range strains.Data {
-			_, err := ann.GetEntryAnnotation(
-				ctx,
-				&anno.EntryAnnotationRequest{
-					Tag:      "strain_inventory",
-					Ontology: "strain_inventory",
-					EntryId:  n.Id,
-				},
-			)
-			if err != nil {
-				unavail++
-			} else {
-				avail++
-			}
-		}
-		elapsed := time.Since(start)
-		log.Printf("query took %s", elapsed)
+	config := &query.Config{
+		StockClient: st,
+		AnnoClient:  ann,
+		Quantity:    c.Int("quantity"),
 	}
-	elapsed := time.Since(start)
-	log.Printf("full execution took %s", elapsed)
-	log.Printf("total number of available strains: %d", avail)
-	log.Printf("total number of unavailable strains: %d", unavail)
+	q, err := query.GetRegularInventory(config)
+	if err != nil {
+		return err
+	}
+	log.Printf("full execution took %s", q.Elapsed)
+	log.Printf("total number of available strains: %d", q.Available)
+	log.Printf("total number of unavailable strains: %d", q.Unavailable)
 	return nil
 }
---
+++ internal/query/reginv.go
@@ -0,0 +1,60 @@
+package query
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/dictyBase/go-genproto/dictybaseapis/annotation"
+	"github.com/dictyBase/go-genproto/dictybaseapis/stock"
+)
+
+type Config struct {
+	StockClient stock.StockServiceClient
+	AnnoClient  annotation.TaggedAnnotationServiceClient
+	Quantity    int
+	Query       string
+}
+
+type Analytics struct {
+	Elapsed     time.Duration
+	Available   int
+	Unavailable int
+}
+
+func GetRegularInventory(config *Config) (*Analytics, error) {
+	ctx := context.Background()
+	start := time.Now()
+	avail := 0
+	unavail := 0
+	for v := 10; v <= config.Quantity; v += 10 {
+		start := time.Now()
+		strains, err := config.StockClient.ListStrains(ctx, &stock.StockParameters{Cursor: 0, Limit: 10, Filter: "name!~GWDI;label!=AX4"})
+		if err != nil {
+			return &Analytics{}, err
+		}
+		for _, n := range strains.Data {
+			_, err := config.AnnoClient.GetEntryAnnotation(
+				ctx,
+				&annotation.EntryAnnotationRequest{
+					Tag:      "strain_inventory",
+					Ontology: "strain_inventory",
+					EntryId:  n.Id,
+				},
+			)
+			if err != nil {
+				unavail++
+			} else {
+				avail++
+			}
+		}
+		elapsed := time.Since(start)
+		log.Printf("query took %s", elapsed)
+	}
+	elapsed := time.Since(start)
+	return &Analytics{
+		Elapsed:     elapsed,
+		Available:   avail,
+		Unavailable: unavail,
+	}, nil
+}
